88c43efcc23b368e8600a4bab88cfd4c782df557
pulltheflower <pulltheflower@163.com>
2020-10-10 10:31:15 +0800

    add method

--- app/models/me_page.rb
+++ app/models/me_page.rb
@@ -6,4 +6,9 @@ class MePage
   def test_method_for_me_page_version_2
     'test method for me page version 2'
   end
+
+  def test_method_for_me_page_version_3
+    'test method for me page version 3'
+  end
+  
 end
